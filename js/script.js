2f1bbc0c5a3af9636038e983f4e975beb2c1e216
Your Name <almannofrancesco@gmail.com>
2023-01-09 16:49:55 +0100

    api in progress

--- js/script.js
+++ js/script.js
@@ -10,4 +10,9 @@ createApp({
            mail: null
         }
     },
+    mounted() {
+        axios.get(' https://flynn.boolean.careers/exercises/api/random/mail').then((result)=> {
+            this.email = result.data.response
+        })
+    },
 },).mount('#app')
